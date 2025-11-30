e881d0fdb405f29bc4eb5eceaa3b45d1c92d9d0d
Pierre Humblot-Ferrero <phf@grand-shooting.com>
2025-11-30 10:40:22 +0100

    Simplify database schema exports to fix build errors

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,7 +1,7 @@
 export { getDb, resetDb } from './client';
-
-// Export the correct schemas based on database type (PostgreSQL or SQLite)
-export * from './schemas';
+export * from './schema/digests';
+export * from './schema/admin';
+export * from './schema/templates';
 
 // IMPORTANT: Do not export a pre-initialized db instance here!
 // It would be called at module load time, before environment variables are loaded.
